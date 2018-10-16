21fa126577b3a183948f3ddf331a0023d2dfb01f
DoobyWizard <44184954+DoobyWizard@users.noreply.github.com>
2018-10-16 16:14:20 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ bot = commands.Bot(command_prefix='$$$')
 
 @bot.event
 async def on_ready():
-    print('Logged in as')
+    print('Logged in as: ')
     print(bot.user.name)
     print(bot.user.id)
     print('-----')
